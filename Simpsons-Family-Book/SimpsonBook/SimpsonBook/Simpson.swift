343153ffb11dd71e83a4ea9663fc7959b665fe32
Satyajit-99 <144027927+Satyajit-99@users.noreply.github.com>
2024-09-13 07:13:21 +0530

    Add files via upload

---
+++ Simpsons-Family-Book/SimpsonBook/SimpsonBook/Simpson.swift
@@ -0,0 +1,25 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Satyajit Bhol on 19/08/24.
+//
+
+import Foundation
+import UIKit
+
+
+class Simpson{
+    
+    var name: String
+    var job: String
+    var image: UIImage
+    
+    init(SimpsonName: String, SimpsonJob: String, SimpsonImage: UIImage) {
+        name = SimpsonName
+        job = SimpsonJob
+        image = SimpsonImage
+    }
+    
+    
+}
